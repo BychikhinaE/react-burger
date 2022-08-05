ba258728ff129f7ceef357553327bf0c3ad71adb
Katerina Bychikhina <orodiavoletto@yandex.ru>
2022-08-05 07:33:11 +0300

    feat react components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-//import logo from './logo.svg';
+
 import './App.css';
 import AppHeader from './components/app-header/app-header';
 import BurgerIngredients from './components/burger-ingredients/burger-ingredients';
 import BurgerConstructor from './components/burger-constructor/burger-constructor';
+import arrayIngredients from "./utils/data";
 
 function App() {
   return (
-    <div className="App p-10">
+    <div className="p-10">
     <AppHeader/>
     <main className="main">
-    <BurgerIngredients />
-    <BurgerConstructor />
+    <BurgerIngredients array={arrayIngredients}/>
+    <BurgerConstructor array={arrayIngredients}/>
     </main>
 
     </div>
--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import ingredientPropTypes from "../../utils/ingredientPropTypes";
 
 import {
   Tab,
@@ -7,25 +8,31 @@ import {
   Counter,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from "./burger-ingredients.module.css";
-import arrayIngredients from "../../utils/data";
-const buns = arrayIngredients.filter((item) => item.type === "bun");
-const sauces = arrayIngredients.filter((item) => item.type === "sauce");
-const mains = arrayIngredients.filter((item) => item.type === "main");
 
-const returnMenu=(item: { __v: number; name: string ; image: string | undefined; price: any; }, index: React.Key | null )=>{
+//Вернет разделы меню взависимости от группы продукта
+const ReturnMenu = ({ array, ingredientGroup }) => {
+  const currentObject = array.filter((item) => item.type === ingredientGroup);
   return (
-  <li className={`${styles.item} mb-10`} key={index}>
-  {item.__v > 0 && <Counter count={item.__v} size="default" />}
-  <img alt={item.name} src={item.image} />
-  <div className={styles.price}>
-    <p className="text text_type_digits-default pt-2 pb-3">{item.price}</p>
-    <CurrencyIcon type="primary" />
-  </div>
-  <p className="text text_type_main-default pb-5">{item.name}</p>
-</li>)
-}
+    <ul className={`${styles.list} pr-2 pl-4 pb-10`}>
+      {currentObject.map((item, index) => (
+        <li className={`${styles.item} mb-10`} key={index}>
+          {item.__v > 0 && <Counter count={item.__v} size="default" />}
+          <img alt={item.name} src={item.image} />
+          <div className={styles.price}>
+            <p className="text text_type_digits-default pt-2 pb-3">
+              {item.price}
+            </p>
+            <CurrencyIcon type="primary" />
+          </div>
+          <p className="text text_type_main-default pb-5">{item.name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
-const BurgerIngredients = () => {
+//Отрисует заголовок и всё меню
+const BurgerIngredients = ({ array }) => {
   const [current, setCurrent] = React.useState("Булки");
 
   return (
@@ -67,39 +74,37 @@ const BurgerIngredients = () => {
               Начинки
             </Tab>
           </a>
+
         </nav>
         <div className={`${styles.scroll} custom-scroll`}>
           <h2 className="text text_type_main-medium pb-6" id="buns">
             Булки
           </h2>
-          <ul className={`${styles.list} pr-2 pl-4 `}>
-            {buns.map((item, index) =>  returnMenu(item, index)
-            )}
-          </ul>
+          <ReturnMenu array={array} ingredientGroup={"bun"} />
 
           <h2 className="text text_type_main-medium pb-4" id="souce">
             Соусы
           </h2>
-          <ul className={`${styles.list} pr-2 pl-4 pb-10`}>
-            {sauces.map((item, index) => returnMenu(item, index)
-            )}
-          </ul>
+          <ReturnMenu array={array} ingredientGroup={"sauce"} />
 
           <h2 className="text text_type_main-medium pb-6" id="mains">
             Начинки
           </h2>
-          <ul className={`${styles.list} pr-2 pl-4 pb-10`}>
-            {mains.map((item, index) => returnMenu(item, index)
-            )}
-          </ul>
+          <ReturnMenu array={array} ingredientGroup={"main"} />
+
         </div>
       </section>
     </>
   );
 };
 
-export default BurgerIngredients;
-
 BurgerIngredients.propTypes = {
-
+  array: PropTypes.arrayOf(ingredientPropTypes),
 };
+
+ReturnMenu.propTypes = {
+  array: PropTypes.arrayOf(ingredientPropTypes),
+  ingredientGroup: PropTypes.string.isRequired,
+};
+
+export default BurgerIngredients;
